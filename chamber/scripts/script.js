85ad59b845762162fea32693fb2d9c8e433e3d92
Tagg McDonald <taggmcd@gmail.com>
2023-10-14 13:43:19 -0600

    Add dark mode

---
+++ chamber/scripts/script.js
@@ -0,0 +1,30 @@
+var rootElement = document.querySelector(":root");
+var cardElements = document.querySelectorAll(".card");
+const modeElement = document.querySelector("#mode");
+
+function darkMode() {
+  rootElement.style.setProperty("--color-para-bg", "#222222");
+  rootElement.style.setProperty("background-color", "#222222");
+  cardElements.forEach(function (cardElement) {
+    cardElement.style.setProperty("background-color", "#222222");
+    cardElement.style.setProperty("color", "white");
+  });
+}
+
+function lightMode() {
+  rootElement.style.setProperty("--color-para-bg", "var(--accent2)");
+  rootElement.style.setProperty("background-color", "white");
+  cardElements.forEach(function (cardElement) {
+    cardElement.style.setProperty("background-color", "white");
+    cardElement.style.setProperty("color", "black");
+  });
+}
+modeElement.addEventListener("click", () => {
+  if (modeElement.textContent.includes("🕶️")) {
+    darkMode();
+    modeElement.textContent = "🔆";
+  } else {
+    lightMode();
+    modeElement.textContent = "🕶️";
+  }
+});
