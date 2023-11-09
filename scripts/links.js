b613a5347830d019f08644736ebe9bc8209c7754
Tagg McDonald <taggmcd@gmail.com>
2023-11-09 08:41:49 -0700

    Add json links and js

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,36 @@
 const baseURL = "https://taggmcd.github.io/wdd230/";
 const linksURL = "https://taggmcd.github.io/wdd230/data/links.json";
+const sectionElement = document.getElementById("activities");
 
 async function getLinks() {
   const response = await fetch(linksURL);
   const data = await response.json();
-  console.log(data);
+  displayLinks(data.lessons);
+  //   console.log(data);
 }
+
+const displayLinks = (lessons) => {
+  let h3Element = document.createElement("h3");
+  h3Element.textContent = "Learning Activities";
+  let ulElement = document.createElement("ul");
+  sectionElement.appendChild(h3Element);
+  sectionElement.appendChild(ulElement);
+
+  lessons.forEach((lesson) => {
+    let liElement = document.createElement("li");
+    let links = lesson.links;
+    liElement.textContent = `${lesson.lesson}: `;
+
+    links.forEach((link) => {
+      let aElement = document.createElement("a");
+      aElement.setAttribute("href", link.url);
+      aElement.textContent = `${link.title} `;
+
+      liElement.appendChild(aElement);
+    });
+    ulElement.appendChild(liElement);
+  });
+  sectionElement.appendChild(ulElement);
+};
 
 getLinks();
